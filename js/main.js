d2fe48ad7bcfd0684789dd0450ccc85d38055b70
Emilio Interlandi <56268536+Dasdar990@users.noreply.github.com>
2020-04-05 19:01:19 +0200

    Quiz V1.0 stable release
    First stable version of the project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function selectQuiz(id) {
+    localStorage.setItem('quizvar', id);
+}
+
+function setQuiz() {
+    document.title = localStorage.getItem('quizvar');
+    const back = document.getElementById('back');
+    back.style.backgroundImage = 'url(images/' + localStorage.getItem('quizvar') + '.jpg)';
+}
