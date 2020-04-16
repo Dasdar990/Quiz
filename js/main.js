fa4a9f16b46bf848c41d40cb406cbfb25878f8a1
Emilio Interlandi <56268536+Dasdar990@users.noreply.github.com>
2020-04-17 01:41:49 +0200

    Quiz V3.0.2 pre-alpha
    Animations fix

--- js/main.js
+++ js/main.js
@@ -15,13 +15,3 @@ function setQuiz() {
 function home() {
     window.location = 'index.html';
 }
-$(document).ready(function() {
-    $('input[type="radio"]').click(function() {
-        $this = $(this);
-        $label = $this.parent();
-        // first make ALL labels normal
-        $label.parent().parent().find('label').css('background', '#f2f2f2');
-        // then color just THIS one
-        $label.css('background', '#e6812f');
-    });
-});
--- js/quiz.js
+++ js/quiz.js
@@ -1,12 +1,9 @@
 window.onload = setQuiz();
+
 $(document).ready(function() {
-    $('input[type="radio"]').click(function() {
-        $this = $(this);
-        $label = $this.parent();
-        // first make ALL labels normal
-        $label.parent().parent().find('label').css('background', '#ccdde7');
-        // then color just THIS one
-        $label.css('background', '#e6812f');
+    $('input[type="radio"').change(function() {
+        $('label.highlighted').removeClass('highlighted');
+        $(this).closest('.answer').addClass('highlighted');
     });
 });
 (function() {})();
