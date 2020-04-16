0c87a380dfcd5b431816424a0cc288ce629bb340
Emilio Interlandi <56268536+Dasdar990@users.noreply.github.com>
2020-04-17 01:00:13 +0200

    Quiz V3.0.2 pre-alpha
    Answer animation added

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,12 @@
 window.onload = setQuiz();
-
+$(document).ready(function() {
+    $('input[type="radio"]').click(function() {
+        $this = $(this);
+        $label = $this.parent();
+        // first make ALL labels normal
+        $label.parent().parent().find('label').css('background', '#ccdde7');
+        // then color just THIS one
+        $label.css('background', '#e6812f');
+    });
+});
 (function() {})();
