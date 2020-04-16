dca1df2cfacfd76dfbabb8d696110417e6a094d0
Emilio Interlandi <56268536+Dasdar990@users.noreply.github.com>
2020-04-16 20:17:41 +0200

    Quiz V3.0.1 pre-alpha

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 function selectQuiz(id) {
-    localStorage.setItem('quizvar', id);
+    var x = document.getElementById(id).querySelectorAll('*');
+
+    localStorage.setItem('title', x[2].innerText);
+    localStorage.setItem('icon', x[1].src);
+    window.location = 'quiz.html';
 }
 
 function setQuiz() {
-    document.title = localStorage.getItem('quizvar');
-    const back = document.getElementById('back');
-    back.style.backgroundImage = 'url(images/' + localStorage.getItem('quizvar') + '.jpg)';
+    document.title = localStorage.getItem('title');
+    document.getElementById('title').innerHTML = localStorage.getItem('title');
+    document.getElementById('icon').src = localStorage.getItem('icon');
 }
 
 function home() {
